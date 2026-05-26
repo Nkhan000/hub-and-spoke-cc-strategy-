588cf1ed7c473e3807ff8b271fc0a93f04a5f591
Your Name <you@email.com>
2026-05-27 00:53:10 +0545

    update error messages

--- src/libraries/errors/VaultErrors.sol
+++ src/libraries/errors/VaultErrors.sol
@@ -19,4 +19,7 @@ library VaultErrors {
         uint256 totalDepositValueUsd,
         uint256 actualIncrease
     );
+    error Vault__lengthMismatch();
+    error Vault__AssetNotSupported(address asset);
+    error Vault__AssetNotActive(address asset);
 }
